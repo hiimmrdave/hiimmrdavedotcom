9f5c9c9e6ac3d7013d7f138f1852f014f9ac4c49
Dave Vogt <horsedreamer@gmail.com>
2023-03-30 22:57:34 -0400

    full language code
    The English that I speak and write is, for better or worse, American.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "hi, i'm Mr. Dave!",
-	url: "https://hiimmrdave.com/",
-	language: "en",
-	description: "i'm Mr. Dave, and this is my website.",
-	author: {
-		name: "Mr. Dave",
-		email: "hi@immrdave.com",
-		url: "https://hiimmrdave.com/about/",
-	},
+  title: "hi, i'm Mr. Dave!",
+  url: "https://hiimmrdave.com/",
+  language: "en-US",
+  description: "i'm Mr. Dave, and this is my website.",
+  author: {
+    name: "Mr. Dave",
+    email: "hi@immrdave.com",
+    url: "https://hiimmrdave.com/about/",
+  },
 };
