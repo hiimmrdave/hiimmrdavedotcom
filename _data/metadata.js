04ce83812cbeb5016511c9a0b97e1f9280c9a093
Dave Vogt <horsedreamer@gmail.com>
2024-12-01 00:30:47 -0500

    updates to metadata and correct xml attributes

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: "hi, i'm Mr. Dave!",
-  url: "https://hiimmrdave.com/",
+  url: "https://www.hiimmrdave.com/",
   language: "en-US",
   description: "i'm Mr. Dave, and this is my website.",
   author: {
     name: "Mr. Dave",
     email: "hi@immrdave.com",
-    url: "https://hiimmrdave.com/about/",
+    url: "https://www.hiimmrdave.com/about/",
   },
 };
