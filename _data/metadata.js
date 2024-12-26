05b7c8cf2eedd0d7647c290b8f45c5f68c305e34
Dave Vogt <horsedreamer@gmail.com>
2024-12-26 01:50:08 -0500

    smash cut to changes from template (kinda)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,11 @@
 export default {
-  // Feel free to override these, they are pulling from package.json for default values.
-  title: "hi, i'm Mr. Dave!",
-  description: "i'm Mr. Dave, and this is my website.",
-  language: "en-US",
-  url: "https://www.hiimmrdave.com",
-  author: {
-    name: "Mr. Dave",
-    email: "hi@immrdave.com",
-    url: "https://www.hiimmrdave.com/about/",
-  },
+	title: "hi, i'm Mr. Dave!",
+	description: "i'm Mr. Dave, and this is my website.",
+	language: "en-US",
+	url: "https://www.hiimmrdave.com",
+	author: {
+		name: "Mr. Dave",
+		email: "hi@immrdave.com",
+		url: "https://www.hiimmrdave.com/about/",
+	},
 };
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,12 @@
-import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+import {
+	IdAttributePlugin,
+	InputPathToUrlTransformPlugin,
+	HtmlBasePlugin,
+} from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
-// import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import pluginInclusiveLang from "@11ty/eleventy-plugin-inclusive-language";
 import pluginMermaid from "@kevingimbel/eleventy-plugin-mermaid";
@@ -10,93 +14,170 @@ import pluginFilters from "./_config/filters.js";
 
 import markdownItFootnote from "markdown-it-footnote";
 import markdownItDeflist from "markdown-it-deflist";
-import markdownItCollapsible from "./markdown-it-collapsible.js";
+import markdownItCollapsible from "./_config/markdown-it-collapsible.js";
 import markdownItAttrs from "markdown-it-attrs";
-import markdownItAnchor from "markdown-it-anchor";
 import markdownItAbbr from "markdown-it-abbr";
 
-/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
-export default function (eleventyConfig) {
-  // Drafts, see also _data/eleventyDataSchema.js
-  eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-    if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
-      return false;
-    }
-  });
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default async function (eleventyConfig) {
+	// Drafts, see also _data/eleventyDataSchema.js
+	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+		if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+			return false;
+		}
+	});
 
-  // Copy the contents of the `public` folder to the output folder
-  // For example, `./public/css/` ends up in `_site/css/`
-  eleventyConfig
-    .addPassthroughCopy({
-      "./_public/": "/",
-    })
-    .addPassthroughCopy("_content/feed/pretty-atom-feed.xsl");
+	// Copy the contents of the `_public` folder to the output folder
+	// For example, `./_public/css/` ends up in `_site/css/`
+	eleventyConfig
+		.addPassthroughCopy({
+			"./_public/": "/",
+		})
+		.addPassthroughCopy("./_content/feed/pretty-atom-feed.xsl");
 
-  eleventyConfig.ignores.add("README.md");
+	// Run Eleventy when these files change:
+	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
-  eleventyConfig.addPlugin(pluginWebc, {
-    components: ["./_components/**/*.webc", "npm:@11ty/is-land/*.webc"],
-  });
-  eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: "rss", // or "atom", "json"
-    outputPath: "/feed.xml",
-    collection: {
-      name: "posts", // iterate over `collections.posts`
-      limit: 0, // 0 means no limit
-    },
-    metadata: {
-      language: "en-US",
-      title: "hi, i'm Mr. Dave!",
-      subtitle: "i'm Mr. Dave, and this is my website.",
-      base: "https://hiimmrdave.com/",
-      author: {
-        name: "Mr. Dave",
-        email: "hi@immrdave.com",
-      },
-    },
-  });
-  eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-    preAttributes: { tabindex: 0 },
-  });
-  eleventyConfig.addPlugin(pluginInclusiveLang);
-  eleventyConfig.addPlugin(pluginMermaid);
+	// Watch content images for the image pipeline.
+	eleventyConfig.addWatchTarget("_content/**/*.{svg,webp,png,jpeg}");
 
-  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+	// Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
+	// Adds the {% css %} paired shortcode
+	eleventyConfig.addBundle("css", {
+		toFileDirectory: "dist",
+	});
+	// Adds the {% js %} paired shortcode
+	eleventyConfig.addBundle("js", {
+		toFileDirectory: "dist",
+	});
 
-  // Customize Markdown library settings:
-  eleventyConfig.amendLibrary("md", (mdLib) => {
-    mdLib
-      .use(markdownItAbbr)
-      .use(markdownItAnchor, {
-        permalink: markdownItAnchor.permalink.ariaHidden({
-          placement: "after",
-          class: "header-anchor",
-          symbol: "#",
-          ariaHidden: false,
-        }),
-        level: [1, 2, 3, 4],
-        slugify: eleventyConfig.getFilter("slugify"),
-      })
-      .use(markdownItAttrs)
-      .use(markdownItCollapsible)
-      .use(markdownItDeflist)
-      .use(markdownItFootnote);
-  });
+	// Official plugins
+	eleventyConfig.addPlugin(pluginWebc, {
+		components: ["./_components/**/*.webc", "npm:@11ty/is-land/*.webc"],
+	});
+	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+		preAttributes: { tabindex: 0 },
+	});
+	eleventyConfig.addPlugin(pluginNavigation);
+	eleventyConfig.addPlugin(HtmlBasePlugin);
+	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
-  eleventyConfig.setServerOptions({
-    domDiff: false,
-  });
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed/feed.xml",
+		stylesheet: "pretty-atom-feed.xsl",
+		/*templateData: {
+			eleventyNavigation: {
+				key: "Feed",
+				order: 4,
+			},
+		},*/
+		collection: {
+			name: "posts",
+			limit: 0,
+		},
+		metadata: {
+			language: "en-US",
+			title: "hi, i'm Mr. Dave!",
+			subtitle: "i'm Mr. Dave, and this is my website.",
+			base: "https://hiimmrdave.com/",
+			author: {
+				name: "Mr. Dave",
+				email: "hi@immrdave.com",
+			},
+		},
+	});
 
-  eleventyConfig.addPlugin(pluginFilters);
+	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// File extensions to process in _site folder
+		extensions: "html",
+
+		// Output formats for each image.
+		formats: ["avif", "webp", "auto"],
+
+		// widths: ["auto"],
+
+		defaultAttributes: {
+			// e.g. <img loading decoding> assigned on the HTML tag will override these values.
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
+	eleventyConfig.addPlugin(pluginInclusiveLang);
+	eleventyConfig.addPlugin(pluginMermaid);
+
+	// Filters
+	eleventyConfig.addPlugin(pluginFilters);
+
+	eleventyConfig.addPlugin(IdAttributePlugin, {
+		// by default we use Eleventy’s built-in `slugify` filter:
+		// slugify: eleventyConfig.getFilter("slugify"),
+		// selector: "h1,h2,h3,h4,h5,h6", // default
+	});
+
+	// Customize Markdown library settings:
+	eleventyConfig.amendLibrary("md", (mdLib) => {
+		mdLib
+			.use(markdownItAbbr)
+			/*.use(markdownItAnchor, {
+				permalink: markdownItAnchor.permalink.ariaHidden({
+					placement: "after",
+					class: "header-anchor",
+					symbol: "#",
+					ariaHidden: false,
+				}),
+				level: [1, 2, 3, 4],
+				slugify: eleventyConfig.getFilter("slugify"),
+			})*/
+			.use(markdownItAttrs)
+			.use(markdownItCollapsible)
+			.use(markdownItDeflist)
+			.use(markdownItFootnote);
+	});
+
+	eleventyConfig.addShortcode("currentBuildDate", () => {
+		return new Date().toISOString();
+	});
+
+	// Features to make your build faster (when you need them)
+
+	// If your passthrough copy gets heavy and cumbersome, add this line
+	// to emulate the file copy on the dev server. Learn more:
+	// https://www.11ty.dev/docs/copy/#emulate-passthrough-copy-during-serve
+
+	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 }
 
 export const config = {
-  dir: {
-    input: "_content", // default: "."
-    includes: "../_includes", // default: "_includes"
-    data: "../_data", // default: "_data"
-  },
+	// Control which files Eleventy will process
+	// e.g.: *.md, *.njk, *.html, *.liquid
+	templateFormats: ["md", "njk", "html", "liquid", "11ty.js", "webc"],
 
-  markdownTemplateEngine: "webc",
+	// Pre-process *.md files with: (default: `liquid`)
+	markdownTemplateEngine: "webc",
+
+	// Pre-process *.html files with: (default: `liquid`)
+	htmlTemplateEngine: "webc",
+
+	// These are all optional:
+	dir: {
+		input: "_content", // default: "."
+		includes: "../_includes", // default: "_includes" (`input` relative)
+		data: "../_data", // default: "_data" (`input` relative)
+		output: "_site",
+	},
+
+	// -----------------------------------------------------------------
+	// Optional items:
+	// -----------------------------------------------------------------
+
+	// If your site deploys to a subdirectory, change `pathPrefix`.
+	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+
+	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
+	// it will transform any absolute URLs in your HTML to include this
+	// folder name and does **not** affect where things go in the output folder.
+
+	// pathPrefix: "/",
 };
